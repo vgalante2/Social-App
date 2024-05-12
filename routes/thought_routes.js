e891e0c16f17756153b733cc3d074810a3edb71d
vgalante2 <vgalante0@gmail.com>
2024-05-12 12:07:59 -0400

    user/thought routes finished

--- routes/thought_routes.js
+++ routes/thought_routes.js
@@ -1,9 +1,11 @@
 const router = require('express').Router()
+const session = require('express-session');
 const { Thought } = require('../model');
 const ObjectId = require('mongodb').ObjectId;
 
 
-router.post('/thought', async (req, res) => {
+// CREATE A NEW THOUGHT
+router.post('/thoughts', async (req, res) => {
     try {
         const thought = await Thought.create(req.body)
 
@@ -20,19 +22,76 @@ router.post('/thought', async (req, res) => {
     
     })
 
+    // GET ALL THOUGHTS
 router.get('/thoughts', async (req, res) => {
-
-
-
-})
-
-
-router.get('/:thoughtId/reactions', async (req, res) => {
-
+   try{
+    const thoughts = await Thought.find()
+    req.session.user_id = thoughts._id
  
-
+     res.json({
+         message: 'Thoughts generated successfully',
+         thoughts
+     })
+   }
+   catch (err) {
+    console.log(err)
+   }
+    
 })
 
+// GET THOUGHT BY ID
+router.get('/thoughts/:id', async (req, res) => {
+    try {
+        const thought = await Thought.findById(req.params.id)
+        req.session.user_id = thought._id
+
+        res.json({
+            message: 'Thought with ID found successfully',
+            thought
+        })
+    }
+    catch (err) {
+        console.log(err)
+    }
+ 
+})
+
+// UPDATE A THOUGHT BY ID/CHANGE WITH JSON
+router.put('/thoughts/:id', async (req, res) => {
+    try {
+        const updatedThought = await Thought.updateOne({
+            _id: new ObjectId(req.params.id)}
+            ,{$set: req.body})
+
+            updatedThought.thoughtText = req.body.thoughtText
+
+            res.json({
+                message: 'Updated thought successfully',
+                updatedThought
+            })
+
+}
+catch (err) {
+    console.log(err)
+}
+})
+
+
+// DELETE A THOUGHT BY ID
+router.delete('/thoughts/:id', async (req, res) => {
+    try {
+        const deletedThought = await Thought.findByIdAndDelete({_id: req.params.id})
+
+        if(!deletedThought) return res.json({message: 'No thought found'})
+
+            res.json({
+                message: 'Deleted thought successfully'
+            })
+    }
+    catch(err) {
+        console.log(err)
+    }
+})
 
 
 
