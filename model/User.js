8c6c520c1f805e53d54192c6f5cb4cc710cf108c
vgalante2 <vgalante0@gmail.com>
2024-05-12 21:54:14 -0400

    added friend routes

--- model/User.js
+++ model/User.js
@@ -14,17 +14,21 @@ const userSchema = new Schema({
         required: true,
 
     },
-    password: {
-        type: String,
-        unique: true,
-        required: true,
-
-    },
     thoughts: {
-    type: Schema.Types.ObjectId,
-    ref: 'Thought'
-    }
+        type: Schema.Types.ObjectId,
+        ref: 'Thought'
+    },
+    friends: [{
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+    }]
+}, {
+    toJSON: {virtuals: true}
+    
+})
 
+userSchema.virtual('friendCount').get(function() { 
+    return this.friends.length
 })
 
 const User = model('User', userSchema);
